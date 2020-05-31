7288de51b1116635a786b4e95111e4ae3f265dd1
Edward Martínez <Alejed@gmail.com>
2020-05-31 15:35:56 -0500

    Solución reto 3 python

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 # Resolve the problem!!
+import re
+
+patron = re.compile(r'[a-z]')
 
 
 def run():
     # Start coding here
+    with open('E:\Cursos\Curso_Basico_Python\Challenges\Challenge3\challenge-python-03\src\encoded.txt', 'r', encoding='utf-8') as f:
+        linea = f.read()
+        
+        mensaje = ''.join(patron.findall(linea))
+        print(f'El mensaje oculto es: {mensaje}')
+
+
 
 
 if __name__ == '__main__':
